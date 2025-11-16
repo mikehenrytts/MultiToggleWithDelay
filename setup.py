a3be5d0efb4d0a9bd07ebeaae1310761279bc9af
mikehenrytts <31904584+mikehenrytts@users.noreply.github.com>
2025-11-16 14:58:26 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from setuptools import setup
 
-setup(name='cbpi4-plugin-MultiToggleWithDelay',
-      version='1.0.0',
-      description='CraftBeerPi Plugin',
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='cbpi4-MultiToggleWithDelay',
+      version='0.1.0',
+      description='CraftBeerPi4 step plugin: multi-actor toggle with per-actor delays',
       author='Mike Henry',
-      author_email='',
-      url='',
+      author_email='mikehenrytts"yahoo.com',
+      url='https://github.com/PiBrewing/cbpi4-pt100x',
       include_package_data=True,
       package_data={
         # If any package contains *.txt or *.rst files, include them:
       '': ['*.txt', '*.rst', '*.yaml'],
-      'cbpi4-plugin-MultiToggleWithDelay': ['*','*.txt', '*.rst', '*.yaml']},
-      packages=['cbpi4-plugin-MultiToggleWithDelay'],
+      'cbpi4-MultiToggleWithDelay': ['*','*.txt', '*.rst', '*.yaml']},
+      packages=['cbpi4-MultiToggleWithDelay'],
+      long_description=long_description,
+      long_description_content_type='text/markdown', 
      )
